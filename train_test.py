ed440bd0d8bdaa9fa64f9e5e20d1157bf962fab4
shoolamahaboola <46501487+shoolamahaboola@users.noreply.github.com>
2022-09-24 15:11:56 +0300

    Added better params

--- train_test.py
+++ train_test.py
@@ -6,6 +6,9 @@ from ResNet import resnet18, ResNet, ResNetBottleNeckBlock
 import torch.nn as nn
 import torch.nn.functional as F
 
+# params = {'epochs': 20, 'batch_size': 128, 'deepths': [8], 'blocks_sizes': [128]} # 98% train, 75% test
+params = {'epochs': 20, 'batch_size': 128, 'deepths': [8], 'blocks_sizes': [128]}
+
 if __name__ == '__main__':
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print(f'{device=}')
@@ -13,7 +16,7 @@ if __name__ == '__main__':
         [transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    batch_size = 4
+    batch_size = params['batch_size']
 
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=transform)
@@ -28,18 +31,18 @@ if __name__ == '__main__':
     classes = ('plane', 'car', 'bird', 'cat',
             'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-    net = ResNet(3, 10, block=ResNetBottleNeckBlock, deepths=[1], blocks_sizes=[64])
+    net = ResNet(3, 10, block=ResNetBottleNeckBlock, deepths=params['deepths'], blocks_sizes=params['blocks_sizes'])
     net.to(device)
-    from torchsummary import summary
+    # from torchsummary import summary
 
     # model = resnet18(3, 1000)
-    summary(net.cuda(), (3, 224, 224))
+    # summary(net.cuda(), (3, 224, 224))
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
     print('Starting training')
-    for epoch in range(2):  # loop over the dataset multiple times
+    for epoch in range(params['epochs']):  # loop over the dataset multiple times
 
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
@@ -59,11 +62,30 @@ if __name__ == '__main__':
 
             # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            if i % 100 == 99:    # print every 200 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 100:.3f}')
                 running_loss = 0.0
 
     print('Finished Training')
+    p = './trained_nets/cifar_net.pth'
+    torch.save(net.state_dict(), p)
+    
+    correct = 0
+    total = 0
+    # since we're not training, we don't need to calculate the gradients for our outputs
+    with torch.no_grad():
+        for data in trainloader:
+            # images, labels = data
+            images, labels = data[0].to(device), data[1].to(device)
+
+            # calculate outputs by running images through the network
+            outputs = net(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f'Accuracy of the network on the train images: {100 * correct // total} %')
 
     correct = 0
     total = 0
